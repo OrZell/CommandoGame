a47e96defd4a42a0a4ccebb501716537dd02b5da
Or Zellinger <or.zellinger@gmail.com>
2025-05-27 17:21:03 +0300

    Complete ColdTools

---
+++ CommandoGame/ColdTools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoGame.Models
+{
+    public class ColdTools
+    {
+        private string _Status;
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+        private int _MaxHits;
+        public int MaxHits
+        {
+            get { return _MaxHits; }
+            set { _MaxHits = value; }
+        }
+
+        private int _CurrentHit;
+        public int CurrentHit
+        {
+            get { return _CurrentHit; }
+            set { _CurrentHit = value; }
+        }
+    }
+}
