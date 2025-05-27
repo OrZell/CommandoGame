91de1a59f246f9bd4ae12d56a8b2760ea40ff1b0
Or Zellinger <or.zellinger@gmail.com>
2025-05-27 12:19:40 +0300

    Enemy Complete

---
+++ CommandoGame/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoGame.Models
+{
+    public class Enemy
+    {
+        private string Name;
+        public int Health;
+        private string Status;
+
+        public Enemy(string name)
+        {
+            this.Name = name;
+            this.Health = 100;
+            this.Status = "Alive";
+        }
+
+        public void Scream()
+        {
+            Console.WriteLine("I'm Enemy");
+        }
+    }
+}
