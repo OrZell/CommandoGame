62e0cde4c220d1974533e3f4240cdf3865e87f6c
Or Zellinger <or.zellinger@gmail.com>
2025-05-27 15:28:34 +0300

    Interfaces Created

---
+++ CommandoGame/Interfaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandoGame.Models
+{
+    interface IShootable
+    {
+        public void Shoot();
+        public int Capacity { get; set; }
+    }
+
+    interface IBreakable
+    {
+        public string Status { get; set; }
+        public int MaxHits { get; set; }
+        public int CurrentHit { get; set; }
+    }
+}
