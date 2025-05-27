4bb54e12fc413a6193a43658b9ee25ea6c7ac265
Or Zellinger <or.zellinger@gmail.com>
2025-05-27 11:28:26 +0300

    SeaCommando Complete

---
+++ CommandoGame/SeaCommando.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandoGame.Models;
+
+namespace CommandoGame.Models
+{
+    public class SeaCommando : Commando
+    {
+        public SeaCommando(string name, string codename) : base(name, codename)
+        {
+
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"SeaCommando CodeName - {this.CodeName} Attacking");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Swiming");
+        }
+    }
+}
