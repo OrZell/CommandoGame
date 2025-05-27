7f7f3d1bd87be1753ad03b2542217f4e37e86083
Or Zellinger <or.zellinger@gmail.com>
2025-05-27 17:14:51 +0300

    Edited thew knife class to be part of ColdTools clas

--- CommandoGame/Knife.cs
+++ CommandoGame/Knife.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace CommandoGame.Models
 {
-    public class Knife : IStabable
+    public class Knife : IBreakable
     {
         public string Name;
-        public string Kind { get; set; }
+        public string Kind;
         public string Creator;
         public string Color;
-        public int Weight { get; set; }
+        public int Weight;
         public string Status { get; set; }
+        public int MaxHits { get; set; }
+        public int CurrentHit { get; set; }
 
         public Knife(string name, string creator, string color, int weight)
         {
@@ -22,11 +24,33 @@ namespace CommandoGame.Models
             this.Color = color;
             this.Weight = weight;
             this.Status = "Complete";
+            this.MaxHits = 5;
+            this.CurrentHit = 0;
         }
 
         public void Stab()
         {
-            Console.WriteLine("Stab");
+            if (this.check())
+            {
+                Console.WriteLine("Stab");
+                if (this.CurrentHit == this.MaxHits-1)
+                {
+                    this.Status = "Broken";
+                }
+            }
+            else
+            {
+                Console.WriteLine("The Knife is Broken");
+            }
+        }
+
+        public bool check()
+        {
+            if (this.CurrentHit < this.MaxHits)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
