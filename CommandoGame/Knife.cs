4cceb162b9eca743fcf175781338060227ab777a
Or Zellinger <or.zellinger@gmail.com>
2025-05-28 10:10:34 +0300

    add CurrentHit++

--- CommandoGame/Knife.cs
+++ CommandoGame/Knife.cs
@@ -17,6 +17,7 @@
             if (this.CheckAbility())
             {
                 Console.WriteLine("Stab");
+                this.CurrentHit++;
                 this.CheckAndUpadteStatus();
             }
             else
