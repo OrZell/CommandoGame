ebc24c58d98356bbd7b7578d1ba27a923fd82620
Or Zellinger <or.zellinger@gmail.com>
2025-05-28 10:02:02 +0300

    Inheritance the coldtools

--- CommandoGame/Knife.cs
+++ CommandoGame/Knife.cs
@@ -2,50 +2,27 @@
 {
     public class Knife : ColdTools, IBreakable
     {
-        public string Name;
         public string Kind;
         public string Creator;
-        public string Color;
-        public int Weight;
-        public string Status { get; set; }
-        public int MaxHits { get; set; }
-        public int CurrentHit { get; set; }
 
-        public Knife(string name, string creator, string color, int weight, string kind)
+        public Knife(string name, string creator, string color, int weight, string kind) : base(name, color, weight)
         {
             this.Name = name;
             this.Creator = creator;
-            this.Color = color;
-            this.Weight = weight;
-            this.Status = "Complete";
-            this.MaxHits = 5;
-            this.CurrentHit = 0;
             this.Kind = kind;
         }
 
         public void Stab()
         {
-            if (this.check())
+            if (this.CheckAbility())
             {
                 Console.WriteLine("Stab");
-                if (this.CurrentHit == this.MaxHits-1)
-                {
-                    this.Status = "Broken";
-                }
+                this.CheckAndUpadteStatus();
             }
             else
             {
-                Console.WriteLine("The Knife is Broken");
+                Console.WriteLine("The Knife Is Broken");
             }
-        }
-
-        public bool check()
-        {
-            if (this.CurrentHit < this.MaxHits)
-            {
-                return true;
-            }
-            return false;
         }
     }
 }
