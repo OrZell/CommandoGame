1da1feb6b547c6a06efad9fa528f6ded91fa7ce6
Or Zellinger <or.zellinger@gmail.com>
2025-05-28 10:08:38 +0300

    inheritance the coldtools

--- CommandoGame/Rock.cs
+++ CommandoGame/Rock.cs
@@ -2,48 +2,23 @@
 {
     public class Rock : ColdTools, IBreakable
     {
-        public string Status { get; set; }
-        public int MaxHits { get; set; }
-        public int CurrentHit { get; set; }
-
-        public string Name;
-        public int Weight;
-        public string Color;
-
-        public Rock(string name, string color, int weight)
+        public Rock(string name, string color, int weight) : base(name, color, weight)
         {
-            this.Name = name;
-            this.Color = color;
-            this.Weight = weight;
-            this.Status = "Complete";
-            this.MaxHits = 5;
-            this.CurrentHit = 0;
+
         }
 
         public void Throw()
         {
-            if (this.Check())
+            if (this.CheckAbility())
             {
-                Console.WriteLine("Throw Rck");
+                Console.WriteLine("Throw Rock");
                 this.CurrentHit++;
-                if (this.CurrentHit == this.MaxHits-1)
-                {
-                    this.Status = "Broken";
-                }
+                this.CheckAndUpadteStatus();
             }
             else
             {
                 Console.WriteLine("The Rock is Broken");
             }
-        }
-
-        private bool Check()
-        {
-            if (this.CurrentHit < this.MaxHits)
-            {
-                return true;
-            }
-            return false;
         }
     }
 }
