3bfbe1d8716ef89b3d8553845f6f37900b9ff64f
Or Zellinger <or.zellinger@gmail.com>
2025-05-28 09:56:29 +0300

    Updated the ColdTools And Add Constractor

--- CommandoGame/ColdTools.cs
+++ CommandoGame/ColdTools.cs
@@ -2,6 +2,27 @@
 {
     public class ColdTools
     {
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private string _Color;
+        public string Color
+        {
+            get { return _Color; }
+            set { _Color = value; }
+        }
+
+        private int _Weight;
+        public int Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+
         private string _Status;
         public string Status
         {
@@ -21,6 +42,35 @@
         {
             get { return _CurrentHit; }
             set { _CurrentHit = value; }
+        }
+
+
+
+        public ColdTools(string name, string color, int weight)
+        {
+            this._Name = name;
+            this._Color = color;
+            this._Weight = weight;
+            this._MaxHits = 5;
+            this._CurrentHit = 0;
+            this._Status = "Complete";
+        }
+
+        public void CheckAndUpadteStatus()
+        {
+            if (this._CurrentHit >= this._MaxHits)
+            {
+                this._Status = "Broken";
+            }
+        }
+
+        public bool CheckAbility()
+        {
+            if (this._CurrentHit < this._MaxHits)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
